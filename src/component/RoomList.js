6f0b418aeeeef94567709a32d5323833e76bb179
Wong Yiu Hang <henrymail2006@ymail.com>
2018-11-30 19:05:11 +0800

    Update: Room list format for better display in mobile

--- src/component/RoomList.js
+++ src/component/RoomList.js
@@ -1,65 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-
-const styles = (theme) => ({
-	root: {
-		...theme.mixins.gutters(),
-		paddingTop: theme.spacing.unit * 2,
-		paddingBottom: theme.spacing.unit * 2
-	}
-});
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
 
 let emptyList = () => (
-	<Typography component="p" align="center">
-		No records
-	</Typography>
+	<ListItem>
+		<ListItemText>
+			<Typography component="p" align="center">
+				No records
+			</Typography>
+		</ListItemText>
+	</ListItem>
 );
 
 let displayList = (list) => (
-	<Table>
-		<TableHead>
-			<TableRow>
-				<TableCell>Room No.</TableCell>
-				<TableCell>Capacity</TableCell>
-				<TableCell>Furniture</TableCell>
-				<TableCell>Available Time</TableCell>
-			</TableRow>
-		</TableHead>
-		<TableBody>
-			{list.map((row, id) => {
-				return (
-					<TableRow key={id}>
-						<TableCell>{row[0]}</TableCell>
-						<TableCell>{row[1]}</TableCell>
-						<TableCell>{row[2]}</TableCell>
-						<TableCell>{row[3]}</TableCell>
-					</TableRow>
-				);
-			})}
-		</TableBody>
-	</Table>
+	<div>
+		{list.map((row, id) => {
+			var divider = (
+				<Divider />
+			);
+			if(id == list.length - 1) {
+				divider = null;
+			}
+			return (
+				<div key={id}>
+					<ListItem>
+						<ListItemText
+							primary={row[0] + ' [' + row[1] + ']'}
+							secondary={row[2]}
+						/>
+						<Typography component="span" align="right">
+							{row[3]}
+						</Typography>
+					</ListItem>
+					{divider}
+				</div>
+			);
+		})}
+	</div>
 );
 
 
 let RoomList = (props) => {
 	return (
-		<Paper className={props.classes.root}>
-			{(props.list.length > 0) ? displayList(props.list) : emptyList()}
+		<Paper>
+			<List>
+				{(props.list.length > 0) ? displayList(props.list) : emptyList()}
+			</List>
 		</Paper>
 	);
 }
 
 RoomList.propTypes = {
-	classes: PropTypes.object.isRequired,
 	list: PropTypes.array.isRequired
 };
 
-export default withStyles(styles)(RoomList);
+export default RoomList;
