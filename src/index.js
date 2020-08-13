8e858b4622011212ff285f42b3c373adff249d1f
Wong Yiu Hang <henrymail2006@ymail.com>
2020-08-14 03:01:13 +0800

    Add: Favicon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {
+	useEffect
+} from 'react'
 import ReactDom from 'react-dom'
+import { renderToString } from 'react-dom/server'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { MuiThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles'
 import { blue, teal } from '@material-ui/core/colors'
 import { CssBaseline, Grid } from '@material-ui/core'
+import { Search as SearchIcon } from '@material-ui/icons'
 import AppBar from './container/AppBarContainer.js'
 import SearchPanelContainer from './container/SearchPanelContainer.js'
 import ResultContainer from './container/ResultContainer.js'
@@ -40,6 +44,22 @@ const useStyles = makeStyles((theme) => ({
 
 let App = (props) => {
 	const classes = useStyles()
+
+	useEffect(() => {
+		const svg = renderToString(<SearchIcon width='16' height='16' xmlns='http://www.w3.org/2000/svg' />)
+		const blob = new Blob([svg], {type: 'image/svg+xml'})
+		const url = URL.createObjectURL(blob)
+
+		const link = document.querySelector('link[rel*=\'icon\']') || document.createElement('link')
+		link.type = 'image/svg+xml'
+		link.rel = 'icon'
+		link.href = url
+		document.getElementsByTagName('head')[0].appendChild(link)
+
+		return () => {
+			URL.revokeObjectURL(url)
+		}
+	}, [])
 
 	return (
 		<Provider store={store}>
